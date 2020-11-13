1d402678480744525a684915541295addc4a2993
yessine <yacinbnsalh@gmail.com>
2020-11-13 09:56:06 +0100

    Composant liste materiel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { Application1Component } from './application1/application1.component';
 import { TransformingPipe } from './transforming.pipe';
 import { AccueilComponent } from './accueil/accueil.component';
+import { ListematrielComponent } from './listematriel/listematriel.component';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { AccueilComponent } from './accueil/accueil.component';
     AppComponent,
     Application1Component,
     TransformingPipe,
-    AccueilComponent
+    AccueilComponent,
+    ListematrielComponent
   
   ],
   imports: [
