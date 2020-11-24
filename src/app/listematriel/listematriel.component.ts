aa2c03f9eb05ec860008c006ea477a6cee7d34bf
yessine <yacinbnsalh@gmail.com>
2020-11-24 18:51:20 +0100

    last version

--- src/app/listematriel/listematriel.component.ts
+++ src/app/listematriel/listematriel.component.ts
@@ -44,7 +44,7 @@ export class ListematrielComponent implements OnInit {
         description: 'PC Portable très pratique', 
         hautGamme: false,
         quantite: 5,
-        commentaires:[ {contenu:' satisfaisant', note:3, auteur:'Sami MRAD', date:new Date(2020,11,4)},
+        commentaires:[ {contenu:' satisfaisant', note:3, auteur:'Youssef ali ', date:new Date(2020,11,4)},
         {contenu:'Bon rapport qualité prix', note:4, auteur:'Samar Salah', date:new Date(2020,10,4)}]
     },
     {   
